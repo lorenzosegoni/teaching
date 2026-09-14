c53f3e0c0a7ecc1c040429eaf1d10f9a629a1796
lorenzosegoni <segonilorenzo2003@gmail.com>
2026-09-14 15:57:36 +0200

    Update Correction.py

--- Test1/Correction.py
+++ Test1/Correction.py
@@ -1,1 +1,23 @@
+## Exercice 1
+# Question 1
+# Question 2a
+# Question 2b
+# Question 3
+# Question 4
 
+## Exercice 2
+# Question 3
+# Question 4
+# Question 5
+# Question 6
+# Question 7
+# Question 8
+# Question 9
+
+## Exercice 3
+# Question 3a
+# Question 3b
+# Question 3c
+# Question 4
+# Question 5
+# Question 6
